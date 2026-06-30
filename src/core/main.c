4ac444b704197b580e1114f9c29c4216e093607e
Aminov Ali <aminovali015@icloud.com>
2026-07-01 00:58:59 +0300

    mouse wheel

--- src/core/main.c
+++ src/core/main.c
@@ -1,3 +1,4 @@
+#include "SDL3/SDL_events.h"
 #define SDL_MAIN_USE_CALLBACKS 1
 
 #include <SDL3/SDL.h>
@@ -47,7 +48,12 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-  if (event->type == SDL_EVENT_QUIT) return SDL_APP_SUCCESS;
+  struct AppContext *ctx = (struct AppContext *)appstate;
+  if (event->type == SDL_EVENT_QUIT) {
+    return SDL_APP_SUCCESS;
+  } else if (event->type == SDL_EVENT_MOUSE_WHEEL) {
+    ctx->mouse.size = CLAMP(ctx->mouse.size + event->wheel.y, 0, MAX_MOUSE_SIZE);
+  }
   return SDL_APP_CONTINUE;
 }
 
--- src/defines.h
+++ src/defines.h
@@ -5,7 +5,11 @@
 #define GRID_WIDTH             611
 #define GRID_HEIGHT            383
 #define FPS                    60
+
 #define MOUSE_COEFFICIENT      0.3f
-#define MAKE_COLOR(r, g, b, a) (((uint32_t)(r) << 24) | ((uint32_t)(g) << 16) | ((uint32_t)(b) << 8) | ((uint32_t)(a)))
+#define MAX_MOUSE_SIZE         50
+
+#define MAKE_COLOR(R, G, B, A) (((uint32_t)(R) << 24) | ((uint32_t)(G) << 16) | ((uint32_t)(B) << 8) | ((uint32_t)(A)))
+#define CLAMP(A, B, C)         ((A) < (B) ? (B) : (A) > (C) ? (C) : (A))
 
 #endif
